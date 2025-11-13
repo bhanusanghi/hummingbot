8f384ad0049482ea6bd4d04d5e4a7e2a79c75eb9
berastotle <berastotle@kodiak.finance>
2025-11-14 01:31:03 +0400

    simplify order_id add back client_order_tracker logs

--- hummingbot/connector/utils.py
+++ hummingbot/connector/utils.py
@@ -67,7 +67,7 @@ def get_new_client_order_id(
     quote = symbols[1].upper()
     base_str = f"{base[0]}{base[-1]}"
     quote_str = f"{quote[0]}{quote[-1]}"
-    client_instance_id = _bot_instance_id()
+    client_instance_id = "" #_bot_instance_id()
     ts_hex = hex(get_tracking_nonce())[2:]
     client_order_id = f"{hbot_order_id_prefix}{side}{base_str}{quote_str}{ts_hex}{client_instance_id}".replace("$", "")
 
